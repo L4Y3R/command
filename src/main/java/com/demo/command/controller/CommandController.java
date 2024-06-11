03980751d47eb6f0269a725a10564279342cd7d2
Nipun Balage <105732109+L4Y3R@users.noreply.github.com>
2024-06-11 12:00:48 +0530

    initial steps

--- src/main/java/com/demo/command/controller/CommandController.java
+++ src/main/java/com/demo/command/controller/CommandController.java
@@ -1,4 +1,20 @@
 package com.demo.command.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/cmd")
 public class CommandController {
+
+    @PostMapping("/open")
+    public String open() {
+        return "Open";
+    }
+
+    @PostMapping("/close")
+    public String close() {
+        return "Close";
+    }
 }
