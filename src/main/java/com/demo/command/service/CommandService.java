59e6e75c8b4054cc07a45489e490ba6f428eaa5c
Nipun Balage <105732109+L4Y3R@users.noreply.github.com>
2024-06-12 09:17:26 +0530

    implemented rest template

--- src/main/java/com/demo/command/service/CommandService.java
+++ src/main/java/com/demo/command/service/CommandService.java
@@ -1,13 +1,15 @@
 package com.demo.command.service;
 
 import com.demo.command.DTO.CommandDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class CommandService {
-    public String control(CommandDTO commandDTO){
-        return "hello";
+
+    public ResponseEntity<CommandDTO> control(CommandDTO commandDTO){
+        return ResponseEntity.ok(commandDTO);
     }
 }
