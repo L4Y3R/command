f7d7e14da4cc628a53cbaf0b1279ca06d95ab7ad
Shanoli Silva <shanoli.silva@zorrosign.com>
2024-06-12 11:08:31 +0530

    Update files

--- src/main/java/com/demo/command/service/CommandService.java
+++ src/main/java/com/demo/command/service/CommandService.java
@@ -1,15 +1,13 @@
 package com.demo.command.service;
 
 import com.demo.command.DTO.CommandDTO;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class CommandService {
-
-    public ResponseEntity<CommandDTO> control(CommandDTO commandDTO){
-        return ResponseEntity.ok(commandDTO);
+    public String control(CommandDTO commandDTO){
+        return "hello";
     }
 }
