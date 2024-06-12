4f4c881a38824915d8b98100272bf73cdb23db5a
Nipun Balage <105732109+L4Y3R@users.noreply.github.com>
2024-06-12 09:36:38 +0530

    -

--- src/main/java/com/demo/command/entity/Command.java
+++ src/main/java/com/demo/command/entity/Command.java
@@ -1,24 +1,2 @@
-package com.demo.command.entity;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.annotation.Id;
-
-import javax.persistence.Entity;
-
-@Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Document(collection = "command")
-
-public class Command {
-    @Id
-    private String cmdName;
-    private String userId;
-    private String DeviceId;
+package com.demo.command.entity;public class Command {
 }
