e72b3317915b071d157a71111dfde7061a0ae156
Nipun Balage <105732109+L4Y3R@users.noreply.github.com>
2024-06-25 09:42:44 +0530

    centralized logging implemented

--- src/main/java/com/demo/command/controller/CommandController.java
+++ src/main/java/com/demo/command/controller/CommandController.java
@@ -3,6 +3,8 @@ package com.demo.command.controller;
 import com.demo.command.DTO.CommandDTO;
 import com.demo.command.exception.CommandNotSuccess;
 import com.demo.command.service.CommandService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,12 +15,16 @@ public class CommandController {
     @Autowired
     private CommandService commandService;
 
+    private static final Logger logger = LoggerFactory.getLogger(CommandController.class);
+
     @PostMapping("/cmd")
     public String cmdRequest(@RequestBody CommandDTO commandDTO) {
         try{
+            logger.info("Command Received");
             commandService.control(commandDTO);
             return "Command processed successfully";
         }catch(Exception e) {
+            logger.error("Command Error");
             throw new CommandNotSuccess();
         }
     }
